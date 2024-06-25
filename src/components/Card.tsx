5dd3f29308bf0c3ee92089039f4484bac026b707
Josh Brown <joshuabrown@hey.com>
2024-06-25 11:09:45 -0600

    feat: apollo

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,42 +1,53 @@
-import { useQuery } from "react-query";
+import { gql, useQuery, useSuspenseQuery } from "@apollo/client";
 
 type Pokemon = {
   id: number;
   name: string;
-  sprites: {
-    front_default: string;
-  };
+  pokemon_v2_pokemonsprites: {
+    sprites: {
+      front_default: string;
+    };
+  }[];
 };
 
-const fetchPokemon = async (id: number): Promise<Pokemon> => {
-  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then(
-    (res) => res.json()
-  );
-  await new Promise((resolve) => setTimeout(resolve, 1000 * id));
-  return res;
-};
+const query = gql`
+  query GetPokemon($id: Int!) {
+    pokemon_v2_pokemon(where: { id: { _eq: $id } }) {
+      id
+      name
+      pokemon_v2_pokemonsprites(limit: 1) {
+        sprites
+      }
+    }
+  }
+`;
 
 export const Card = ({ id }: { id: number }) => {
-  const { data } = useQuery<Pokemon>(["pokemon", id], () => fetchPokemon(id), {
-    suspense: true,
+  const { data } = useQuery<{ pokemon_v2_pokemon: Pokemon[] }>(query, {
+    variables: { id },
   });
 
   return (
     <div className="bg-slate-700 rounded-lg shadow-lg overflow-hidden w-64">
       <div className="relative">
         <img
-          src={data?.sprites?.front_default}
+          src={
+            data?.pokemon_v2_pokemon[0].pokemon_v2_pokemonsprites[0].sprites
+              .front_default
+          }
           alt="Pokémon"
           width={300}
           height={300}
           className="w-full h-64 object-cover"
         />
         <div className="absolute top-2 right-2 bg-muted px-2 py-1 rounded-md text-xs text-muted-foreground">
-          #00{data?.id}
+          #00{data?.pokemon_v2_pokemon[0].id}
         </div>
       </div>
       <div className="p-4">
-        <h3 className="text-xl font-bold text-center mb-2">{data?.name}</h3>
+        <h3 className="text-xl font-bold text-center mb-2">
+          {data?.pokemon_v2_pokemon[0].name}
+        </h3>
       </div>
     </div>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
-const queryClient = new QueryClient();
+const client = new ApolloClient({
+  uri: "https://beta.pokeapi.co/graphql/v1beta",
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
+    <ApolloProvider client={client}>
       <App />
-    </QueryClientProvider>
+    </ApolloProvider>
   </React.StrictMode>
 );
